681aa65bf2562ea7f45c0e9c9990bf034ba72fe5
snowofcloud <41015362+snowofcloud@users.noreply.github.com>
2018-12-26 23:56:52 +0800

    Update Test.java

--- Test.java
+++ Test.java
@@ -15,5 +15,6 @@ public class Test {
         String upcase = upcase(test);
         System.out.println(upcase);
     }
-
+    //String类中每一个看起来会修改String值的方法，实际上都是创建了一个新的对象，并包含修改后的字符串内容，而最初的对象丝毫未动。
+    //每当把String对象作为方法的参数时，都会复制一份引用，而该引用所指的对象其实一直都待在单一物理位置上，从未动过。
 }
