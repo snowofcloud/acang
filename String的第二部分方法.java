878a0e788e8502677337a60cf2ccaa8daf6f915a
snowofcloud <41015362+snowofcloud@users.noreply.github.com>
2018-12-30 13:27:57 +0800

    Update String的第二部分方法.java
    更新hashCode、concat、format方法

--- String的第二部分方法.java
+++ String的第二部分方法.java
@@ -9,17 +9,20 @@ import java.util.Locale;
 public class Bitch {
 
        public static void main(String[] args) {
-        //字符串的split、trim 、contains、valueOf、toString、format、concat、replace、matches、join、copyValueOf、intern、hashCode
+        //字符串的split、trim 、contains、valueOf、toString、format、concat、hashCode、replace、matches、join、intern、copyValueOf
         //首先String不属于8种基本数据类型，String是一个对象。
         String s1 = "low_salary";
         String s2 = "low";
         String s3 = "LOW_SALARY";
         String s4 = "low_salary";
-
+              
+        split
         System.out.println(s1.split("w"));//字符串长度截取-------------底层方法如下，下同。
-       /* public String[] split(String regex) {
+        /* public String[] split(String regex) {
             return split(regex, 0);
-        }*/
+         }*/
+              
+        trim
         System.out.println(s1.trim());//去掉字符串两端的空白（头尾的空白）
         /*public String trim() {
             int len = value.length;
@@ -34,20 +37,26 @@ public class Bitch {
             }
             return ((st > 0) || (len < value.length)) ? substring(st, len) : this;
         }*/
-
+              
+        valueOf
         System.out.println(String.valueOf(s1));//将s1转为字符串
         /*public static String valueOf(Object obj) {
             return (obj == null) ? "null" : obj.toString();
         }*/
-
+              
+        contains
         System.out.println(s1.contains("s"));//判断左边的字符串是否包含右边的内容，返回true/false
         /*public boolean contains(CharSequence s) {
             return indexOf(s.toString()) > -1;
         }*/
+        
+        toString
         System.out.println(s1.toString());//将s1重写字符串
        /* public String toString() {
             return this;
         }*/
+              
+        format
         System.out.println(s1.format());//判断s1是否以y结尾
         //1 占位符
         //1.1 单个占位符
@@ -62,6 +71,7 @@ public class Bitch {
         //format(String format, Object... args) 新字符串使用本地语言环境，制定字符串格式和参数生成格式化的新字符串。
         //format(Locale locale, String format, Object... args) 使用指定的语言环境，制定字符串格式和参数生成格式化的字符串。
 
+        concat
         String s = s1.concat("s");   //concat() 方法用于将指定的字符串参数连接到字符串上。
         //concat：将指定字符串连接到此字符串的结尾。如果参数字符串的长度为 0，则返回此 String 对象。
         //否则，创建一个新的 String 对象，用来表示由此 String，对象表示的字符序列和参数字符串表示的字符序列连接而成的字符序列。
@@ -76,7 +86,8 @@ public class Bitch {
             str.getChars(buf, len);
             return new String(buf, true);
         }*/
-
+            
+        replace
         System.out.println(s3.replace(char oldchar,char newchar));//转小写
         //意思将这个字符串中的所有的oldchar全部换成newchar，并返回一个新的字符串
         /*public String replace(char oldChar, char newChar) {
@@ -106,6 +117,7 @@ public class Bitch {
                 return this;
           }*/
 
+        matches
         System.out.println(s1.matches("{d}"));//和正则表达式匹配
         //String的matches()方法,split()方法中使用正则表达式.
         /*
@@ -114,7 +126,7 @@ public class Bitch {
          }
          */
 
-
+        join
         System.out.println(s1.join(A (String), B (String[])));//和正则表达式匹配
         /*
         String.Join 方法 (A (String), B (String[]));
@@ -124,14 +136,17 @@ public class Bitch {
         此时jn=”abc-def-ghi”;
         */
 
+        intern
         System.out.println(s1.intern());//内容较多，篇幅较大，单都更新
         /*
           public native String intern();
         */
 
+        hashCode
         System.out.println(s1.hashCode());
-        //在String类中有个私有实例字段hash表示该串的哈希值，在第一次调用hashCode方法时，
-        // 字符串的哈希值被计算并且赋值给hash字段，之后再调用hashCode方法便可以直接取hash字段返回。
+        //在String类中有个私有实例字段hash表示该串的哈希值，在第一次调用hashCode方法时，字符串的哈希值被计算并且赋值给hash字段，
+        //之后再调用hashCode方法便可以直接取hash字段返回。
+        //hashCode()方法如下：
         /*public int hashCode() {
             int h = hash;
             if (h == 0 && value.length > 0) {
@@ -144,6 +159,14 @@ public class Bitch {
             }
             return h;
         }*/
+        //String类中的hashCode计算方法还是比较简单的，就是以31为权，每一位为字符的ASCII值进行运算，用自然溢出来等效取模。
+        //哈希计算公式可以计为s[0]*31^(n-1) + s[1]*31^(n-2) + ... + s[n-1]
+        //关于为什么取31为权，主要是因为31是一个奇质数，所以31*i=32*i-i=(i<<5)-i，这种位移与减法结合的计算相比一般的运算快很多。
+        //字符串哈希可以做很多事情，通常是类似于字符串判等，判回文之类的。
+        //但是仅仅依赖于哈希值来判断其实是不严谨的，除非能够保证不会有哈希冲突，通常这一点很难做到。
+   //就拿jdk中String类的哈希方法来举例，字符串"gdejicbegh"与字符串"hgebcijedg"具有相同的hashCode()返回值-801038016，并且它们具有reverse的关系。
+        //这个例子说明了用jdk中默认的hashCode方法判断字符串相等或者字符串回文，都存在反例。
+        //
 
     }
 
