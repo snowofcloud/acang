344143637a0a5987b4bafdc7c3d05e47d1b2ab28
snowofcloud <41015362+snowofcloud@users.noreply.github.com>
2018-12-30 17:44:27 +0800

    String的intern、format方法

--- String的第二部分方法.java
+++ String的第二部分方法.java
@@ -52,19 +52,40 @@ public class Bitch {
         
         toString
         System.out.println(s1.toString());//将s1重写字符串
-       /* public String toString() {
+        /* public String toString() {
             return this;
-        }*/
+         }*/
               
         format
         System.out.println(s1.format());//判断s1是否以y结尾
         //1 占位符
-        //1.1 单个占位符
-        //String s = String.format("Hello %s，Welcome to my blog!", "jerry");
-        //System.out.println(s);//Hello jerry，Welcome to my blog!
-        //1.2 多个占位符
-        //
-        //
+           //1.1 单个占位符
+              //String s = String.format("Hello %s，Welcome to my blog!", "jerry");
+              //System.out.println(s);//Hello jerry，Welcome to my blog!
+          //1.2 多个占位符
+             //1.2.1 按顺序
+               //String s = String.format("Hello %s%s%s", "jerry-", "li", ",welcome!");
+            //1.2.2 按索引
+              //String s = String.format("Hello %2$s%3$s%1$s", "li-", "heng-", "jie");
+        //2 对整数格式化
+              //int n2 = 9999999;
+              //System.out.printf("%+(,d %n", n2);
+           //  - 表示 在最小宽度内左对齐,不可以与0标识一起使用。
+           //  0 表示 若内容长度不足最小宽度
+           //  + 表示 结果总包含一个+或-号。
+           // 空格 表示    正数前加空格 
+           //  ,       只用与十进制
+           //  (      若结果为负数
+       //3 对浮点数格式化
+           //double n = 3.1415926;
+           //String s = String.format("%f %n", num);
+      //4 对日期、时间格式化
+           //对日期格式化的时候不能像对数字那样多种格式对应一个数字，分为日期格式化及时间格式化两部分，如果希望格式化之后的格式是：     
+          // 2017-01-13 12:05:30,需要先格式化日期，然后再格式化时间。    
+          //    Date date = new Date();
+           //   String dateFormat = String.format("%tF %tT ", date, date);
+           //   System.out.println(dateFormat);
+       //底层源码
         /*public boolean endsWith(String suffix) {
             return startsWith(suffix, value.length - suffix.value.length);
         }*/
@@ -131,13 +152,28 @@ public class Bitch {
         /*
         String.Join 方法 (A (String), B (String[]));
         在指定 String 数组B的每个元素之间串联指定的分隔符 A，从而产生单个串联的字符串
-        string [] tmpStr={abc,def,ghi};
+        如下所示：string [] tmpStr={abc,def,ghi};
         string jn = string.Join(“-“, tmpStr);
         此时jn=”abc-def-ghi”;
         */
 
         intern
         System.out.println(s1.intern());//内容较多，篇幅较大，单都更新
+        /*在String类中，intern方法是一个本地方法，intern方法会在运行时常量池中查找是否存在内容相同的字符串，
+          如果存在则返回指向该字符串的引用，如果不存在，则会将该字符串入池，并返回一个指向该字符串的引用。
+          
+          jdk源码中对intern方法的详细解释。简单来说就是intern用来返回常量池中的某字符串，如果常量池中已经存在该字符串，
+          则直接返回常量池中该对象的引用。否则，在常量池中加入该对象，然后 返回引用。
+          
+          String设计的初衷：
+              1. 字符串常量池的需要。字符串常量池的诞生是为了提升效率和减少内存分配。可以说我们编程有百分之八十的时间在处理字符串，
+              而处理的字符串中有很大概率会出现重复的情况。正因为String的不可变性，常量池很容易被管理和优化。
+              2. 安全性考虑。正因为使用字符串的场景如此之多，所以设计成不可变可以有效的防止字符串被有意或者无意的篡改。
+              从java源码中String的设计中我们不难发现，该类被final修饰，同时所有的属性都被final修饰，在源码中也未暴露任何成员变量的修改方法。
+              （当然如果我们想，通过反射或者Unsafe直接操作内存的手段也可以实现对所谓不可变String的修改）。
+              3. 作为HashMap、HashTable等hash型数据key的必要。因为不可变的设计，jvm底层很容易在缓存String对象的时候缓存其hashcode，
+              这样在执行效率上会大大提升。  
+        */
         /*
           public native String intern();
         */
